2262260455f142138d5f4da4be6815e8807761e5
beem10101 <beem25301@gmail.com>
2022-04-11 23:34:04 +0700

    openpyxl ref

---
+++ python_xcel1.py
@@ -0,0 +1,25 @@
+from openpyxl import load_workbook
+
+wb = load_workbook("North.xlsx")
+
+ws = wb['North']
+
+colum_a = ws['A']
+colum_b = ws['B']
+
+row = ws[1]
+
+# print(wb,ws)
+# print(colum_a)
+# print(colum_a[1].value)
+
+for i in row : #or colum
+    print(i.value)
+
+# for i in colum_b :
+#     i.value = i.value * 10
+#     print(i.value)
+
+# wb.save('new_northx10.xlsx') #save new file
+
+
