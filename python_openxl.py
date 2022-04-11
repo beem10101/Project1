d818b2a711f0861abd8386f625590ad460bedd01
beem10101 <beem25301@gmail.com>
2022-04-12 00:09:45 +0700

    11/4/22 openpyxl

---
+++ python_openxl.py
@@ -0,0 +1,21 @@
+import tkinter as tk
+import openpyxl
+from openpyxl import load_workbook
+from tkinter import *
+
+
+root = tk.Tk()
+wb = load_workbook("North.xlsx")
+ws = wb["North"]
+
+colum_a = ws['A']
+colum_b = ws['B']
+
+row = ws[1]
+
+for i in range(1,len(colum_a)+1) :
+    for j in range(len(row)) :
+        Label(root,text=row[j].value).grid(column=j,row=i)
+    row = ws[1+i]
+
+root.mainloop()
